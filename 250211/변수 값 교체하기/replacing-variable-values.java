f5b576639b4caf58812cea5a3bc3d4827f37db51
ethankim <67469416+hey00507@users.noreply.github.com>
2025-02-11 08:43:17 +0900

    Title: 변수 값 교체하기, Time: 87ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 250211/변수 값 교체하기/replacing-variable-values.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+
+        int a = 3;
+        a = 6;
+
+        System.out.print(a);
+    }
+}
