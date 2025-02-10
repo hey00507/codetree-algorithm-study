07c6303b5dc0727adab61c28500b51de871457de
ethankim <67469416+hey00507@users.noreply.github.com>
2025-02-11 08:48:50 +0900

    Title: 정수 복사, Time: 88ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 250211/정수 복사/copy-integer.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+
+        int a = 3;
+        int b = 4;
+
+        b = a;
+        
+        System.out.print(a +" " + b + "\n");
+        System.out.print(a*b);
+    }
+}
