387891977efa168be47693f57e03badb3bec0faa
ethankim <67469416+hey00507@users.noreply.github.com>
2025-02-11 15:36:41 +0900

    Title: 실수 받아 그대로 출력, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 250211/실수 받아 그대로 출력/enter-real-value-and-print.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextDouble();
+        sc.close();
+        System.out.print(a + 0.58);
+    }
+}
